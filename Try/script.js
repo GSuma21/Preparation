db416b7b522a0c3d75bd1a53a6b5d57abf3db13e
suma <21gsuma@gmail.com>
2022-06-10 18:14:25 +0530

    only for preparation

---
+++ Try/script.js
@@ -0,0 +1,7 @@
+console.log("Suma")
+const API = '563492ad6f917000010000013276be18c9f74df78deb58623ac76ef6'
+// fetch(`https://api.imgur.com/3/image/84d877ca5850a65`)
+
+url = 'https://api.pexels.com/v1/'
+
+fetch(API)
